10a6494dd79c8c28c3c42856f134f8f0ed883689
Tadeas Burda <tadeas.burda@sensoneo.com>
2025-03-22 17:06:09 +0100

    Add window position saving and restoring functionality
    This update introduces the ability to save and restore the application's window position using local settings. A constant key is defined for storage, and methods are implemented to save the position when the window closes and to set the position when the application starts, if a saved position exists.

--- Demo WinUI settings module/Demo.App/Demo.App/MainWindow.xaml.cs
+++ Demo WinUI settings module/Demo.App/Demo.App/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.UI.Xaml;
+using Windows.Storage;
 
 namespace Demo.App;
 
@@ -7,8 +8,45 @@ namespace Demo.App;
 /// </summary>
 public sealed partial class MainWindow : Window
 {
+    private const string WINDOW_POSITION_KEY = "WindowPosition";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainWindow"/> class.
+    /// </summary>
     public MainWindow()
     {
         InitializeComponent();
+        SetWindowPosition();
+        Closed += (_, __) => SaveWindowPosition();
+    }
+
+    /// <summary>
+    /// Saves the current window position to local settings.
+    /// </summary>
+    private void SaveWindowPosition()
+    {
+        var localSettings = ApplicationData.Current.LocalSettings;
+        var position = new { Left = AppWindow.Position.X, Top = AppWindow.Position.Y };
+        localSettings.Values[WINDOW_POSITION_KEY] = $"{position.Left},{position.Top}";
+    }
+
+    /// <summary>
+    /// Sets the window position from local settings if available.
+    /// </summary>
+    private void SetWindowPosition()
+    {
+        var localSettings = ApplicationData.Current.LocalSettings;
+        if (localSettings.Values.TryGetValue(WINDOW_POSITION_KEY, out object? value))
+        {
+            var position = value?.ToString()?.Split(',');
+            if (
+                position?.Length == 2
+                && int.TryParse(position[0], out int left)
+                && int.TryParse(position[1], out int top)
+            )
+            {
+                AppWindow.Move(new Windows.Graphics.PointInt32(left, top));
+            }
+        }
     }
 }
